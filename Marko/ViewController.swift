02e874c00cf3476a8365410a88d83ea23408c78b
Ivan Yashchenko <ivan.profitx@gmail.com>
2024-10-18 18:27:01 +0300

    Initial commit

---
+++ Marko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marko
+//
+//  Created by Ivan on 17.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
